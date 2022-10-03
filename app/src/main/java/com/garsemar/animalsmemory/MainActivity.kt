c4e568e31e41d3d24230795218927fb39c686d36
garsemar <martigarciaserra@gmail.com>
2022-10-03 20:59:42 +0200

    Splash

--- app/src/main/java/com/garsemar/animalsmemory/MainActivity.kt
+++ app/src/main/java/com/garsemar/animalsmemory/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.garsemar.animalsmemory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -11,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         difficulty=findViewById(R.id.difficulty)
 
