ff91ec553efe3dadd135515c52b0ec766d7d0a2c
El vostre nom <usuari@domini.com>
2022-10-05 20:03:59 +0200

    Get diff

--- app/src/main/java/com/garsemar/animalsmemory/GameActivity.kt
+++ app/src/main/java/com/garsemar/animalsmemory/GameActivity.kt
@@ -1,11 +1,17 @@
 package com.garsemar.animalsmemory
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_AnimalsMemory)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game)
+
+        val bundle: Bundle? = intent.extras
+        val selectedDiff = bundle?.getString("selectedDiff")
+
+        println(selectedDiff)
     }
 }
--- app/src/main/java/com/garsemar/animalsmemory/MainActivity.kt
+++ app/src/main/java/com/garsemar/animalsmemory/MainActivity.kt
@@ -1,27 +1,50 @@
 package com.garsemar.animalsmemory
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListAdapter
-import android.widget.Spinner
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
-    var difficulty: Spinner? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_AnimalsMemory)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val play = findViewById<Button>(R.id.play)
+        val difficulty = resources.getStringArray(R.array.Difficulty)
+        val intent = Intent(this, GameActivity::class.java)
+        var selectedDiff: String? = null
 
-
-        difficulty=findViewById(R.id.difficulty)
-
-        val difficulties = arrayOf("Difficulty", "Normal", "Hard")
-        val adaptador: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_spinner_item, difficulties)
-
-        difficulty?.adapter=adaptador
+        val spinner = findViewById<Spinner>(R.id.difficulty)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, difficulty
+            )
+            spinner.adapter = adapter
+        }
+        spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                if(position != 0){
+                    selectedDiff = difficulty[position]
+                }
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                Toast.makeText(this@MainActivity, "Select the difficulty", Toast.LENGTH_SHORT).show()
+            }
+        }
+        play.setOnClickListener {
+            if(selectedDiff == null){
+                Toast.makeText(this@MainActivity, "Select the difficulty", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                intent.putExtra("selectedDiff", selectedDiff)
+                startActivity(intent)
+            }
+        }
     }
-
-
 }
