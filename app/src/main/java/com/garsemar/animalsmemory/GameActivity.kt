5d92c0006f42830fcf41b01b37ef3a99fb4086ea
garsemar <martigarciaserra@gmail.com>
2022-10-17 21:00:16 +0200

    Splash

--- app/src/main/java/com/garsemar/animalsmemory/GameActivity.kt
+++ app/src/main/java/com/garsemar/animalsmemory/GameActivity.kt
@@ -3,8 +3,10 @@ package com.garsemar.animalsmemory
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,9 @@ class GameActivity : AppCompatActivity() {
         val selectedDiff = bundle?.getString("selectedDiff")
         val intent = Intent(this, ResultsActivity::class.java)
         val points = "0"
+        val time = findViewById<TextView>(R.id.tiempo)
+
+        val form = "1000÷(120+25)"
 
         Toast.makeText(this@GameActivity, selectedDiff, Toast.LENGTH_SHORT).show()
 
@@ -26,5 +31,18 @@ class GameActivity : AppCompatActivity() {
             intent.putExtra("selectedDiff", selectedDiff)
             startActivity(intent)
         }
+
+        class UpdateTime : TimerTask() {
+            var myTimer = time
+            override fun run() {
+                //calculate the new position of myBall
+            }
+        }
+
+        val timer = Timer()
+
+        val fps = 40
+        val updateBall: TimerTask = UpdateTime()
+        timer.scheduleAtFixedRate(updateBall, 0, (1000 / fps).toLong())
     }
 }
--- app/src/main/java/com/garsemar/animalsmemory/ResultsActivity.kt
+++ app/src/main/java/com/garsemar/animalsmemory/ResultsActivity.kt
@@ -1,12 +1,15 @@
 package com.garsemar.animalsmemory
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class ResultsActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_AnimalsMemory)
         super.onCreate(savedInstanceState)
@@ -14,12 +17,19 @@ class ResultsActivity : AppCompatActivity() {
 
         val bundle: Bundle? = intent.extras
         val points = bundle?.getString("points")
+        val selectedDiff = bundle?.getString("selectedDiff")
+
         val intentPlay = Intent(this, GameActivity::class.java)
         val intentMenu = Intent(this, MainActivity::class.java)
+
         val playAgain = findViewById<Button>(R.id.playAgain)
         val menu = findViewById<Button>(R.id.menu)
+        val pointsText = findViewById<TextView>(R.id.pointsText)
+
+        pointsText.text = "Score:\n$points points"
 
         playAgain.setOnClickListener {
+            intentPlay.putExtra("selectedDiff", selectedDiff)
             startActivity(intentPlay)
         }
         menu.setOnClickListener {
