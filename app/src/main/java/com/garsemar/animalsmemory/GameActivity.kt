4af0c63c3d274ebe89bb66dcb115e1ea88ed9a09
garsemar <inyaki.jin.7e5@itb.cat>
2022-10-19 20:04:17 +0200

    Game logic

--- app/src/main/java/com/garsemar/animalsmemory/GameActivity.kt
+++ app/src/main/java/com/garsemar/animalsmemory/GameActivity.kt
@@ -2,11 +2,13 @@ package com.garsemar.animalsmemory
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
+import kotlin.random.Random
 
 class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +21,18 @@ class GameActivity : AppCompatActivity() {
         val intent = Intent(this, ResultsActivity::class.java)
         val points = "0"
         val time = findViewById<TextView>(R.id.tiempo)
+        val hard = findViewById<LinearLayout>(R.id.hard)
 
-        val form = "1000÷(120+25)"
+        // 1000÷(120(time)+25(intents))
+
+        if(selectedDiff == "Hard"){
+            hard.visibility = View.VISIBLE
+        }
+        else{
+            hard.visibility = View.GONE
+        }
+
+        game()
 
         Toast.makeText(this@GameActivity, selectedDiff, Toast.LENGTH_SHORT).show()
 
@@ -31,18 +43,29 @@ class GameActivity : AppCompatActivity() {
             intent.putExtra("selectedDiff", selectedDiff)
             startActivity(intent)
         }
+    }
+    fun game(){
+        /*val images = listOf<ImageView>(
+            findViewById(R.id.imageView1),
+            findViewById(R.id.imageView2),
+            findViewById(R.id.imageView3),
+            findViewById(R.id.imageView4),
+            findViewById(R.id.imageView5),
+            findViewById(R.id.imageView6)
+        )*/
+        val pos = generateSequence { Random.nextInt(1,7) }.distinct().toList()
+        println(pos)
+        /*images[pos[0]].setImageResource(R.drawable.mono)
+        images[pos[1]].setImageResource(R.drawable.foca)
+        images[pos[2]].setImageResource(R.drawable.mono)
+        images[pos[3]].setImageResource(R.drawable.capybara)
+        images[pos[4]].setImageResource(R.drawable.foca)
+        images[pos[5]].setImageResource(R.drawable.capybara)*/
 
-        class UpdateTime : TimerTask() {
-            var myTimer = time
-            override fun run() {
-                //calculate the new position of myBall
-            }
-        }
+//        images.random()
 
-        val timer = Timer()
-
-        val fps = 40
-        val updateBall: TimerTask = UpdateTime()
-        timer.scheduleAtFixedRate(updateBall, 0, (1000 / fps).toLong())
+        /*images[0].setOnClickListener {
+            images.setImageResource(R.drawable.mono)
+        }*/
     }
 }
