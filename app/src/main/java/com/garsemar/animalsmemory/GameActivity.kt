7e50cdb02ea5c644a378dbb864df4522659da0ad
garsemar <martigarciaserra@gmail.com>
2022-10-26 20:03:27 +0200

    Game landscape fix

--- app/src/main/java/com/garsemar/animalsmemory/GameActivity.kt
+++ app/src/main/java/com/garsemar/animalsmemory/GameActivity.kt
@@ -6,7 +6,10 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
-import android.widget.*
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class GameActivity : AppCompatActivity() {
@@ -20,15 +23,19 @@ class GameActivity : AppCompatActivity() {
         val intent = Intent(this, ResultsActivity::class.java)
         val points = "0"
         val time = findViewById<TextView>(R.id.tiempo)
-        val hard = findViewById<LinearLayout>(R.id.hard)
+        val hard = listOf<ImageView>(findViewById(R.id.imageView7), findViewById(R.id.imageView8))
 
         // 1000÷(120(time)+25(intents))
 
         if(selectedDiff == "Hard"){
-            hard.visibility = View.VISIBLE
+            hard.forEach {
+                it.visibility = View.VISIBLE
+            }
         }
         else{
-            hard.visibility = View.GONE
+            hard.forEach {
+                it.visibility = View.GONE
+            }
         }
 
         game(selectedDiff)
@@ -46,20 +53,42 @@ class GameActivity : AppCompatActivity() {
     @SuppressLint("UseCompatLoadingForDrawables")
     private fun game(selectedDiff: String?){
         val images = mutableListOf<ImageView>(findViewById(R.id.imageView1), findViewById(R.id.imageView2), findViewById(R.id.imageView3), findViewById(R.id.imageView4), findViewById(R.id.imageView5), findViewById(R.id.imageView6))
-        val drawable = listOf(R.drawable.mono, R.drawable.capybara, R.drawable.foca)
-        val numList = randomNums()
-        val map = mapOf(
-            images[numList[0]] to drawable[0],
-            images[numList[1]] to drawable[1],
-            images[numList[2]] to drawable[2],
-            images[numList[3]] to drawable[0],
-            images[numList[4]] to drawable[1],
-            images[numList[5]] to drawable[2],
-        )
-        if(selectedDiff == "Hard"){
+        val drawable = listOf(R.drawable.mono, R.drawable.capybara, R.drawable.foca, R.drawable.lemur)
+        lateinit var map: MutableMap<ImageView, Int>
+        if(selectedDiff == "Normal"){
+            val numList = randomNums(6)
+            map = mutableMapOf(
+                images[numList[0]] to drawable[0],
+                images[numList[1]] to drawable[1],
+                images[numList[2]] to drawable[2],
+                images[numList[3]] to drawable[0],
+                images[numList[4]] to drawable[1],
+                images[numList[5]] to drawable[2]
+            )
+        }
+        else if(selectedDiff == "Hard"){
             images.add(findViewById(R.id.imageView7))
+            images.add(findViewById(R.id.imageView8))
+            val numList = randomNums(8)
+            map = mutableMapOf(
+                images[numList[0]] to drawable[0],
+                images[numList[1]] to drawable[1],
+                images[numList[2]] to drawable[2],
+                images[numList[3]] to drawable[3],
+                images[numList[4]] to drawable[0],
+                images[numList[5]] to drawable[1],
+                images[numList[6]] to drawable[2],
+                images[numList[7]] to drawable[3]
+            )
+        }
+        var right = 0
+        val difficulty: Int = if(selectedDiff == "Normal"){
+            3
+        } else{
+            4
         }
         val selected = mutableListOf<Map.Entry<ImageView, Int>>()
+
         map.forEach { it ->
             it.key.setOnClickListener { lis ->
                 if(it.key.drawable.constantState == resources.getDrawable( R.drawable._1z_x7ojlvl).constantState){
@@ -67,21 +96,30 @@ class GameActivity : AppCompatActivity() {
                     selected.add(it)
                     if(selected.size == 2){
                         if(selected[0].value != selected[1].value){
+                            map.forEach {
+                                it.key.isClickable = false
+                            }
                             Handler(Looper.getMainLooper()).postDelayed({
-                                selected[0].key.setImageResource(R.drawable._1z_x7ojlvl)
-                                selected[1].key.setImageResource(R.drawable._1z_x7ojlvl)
+                                selected.forEach {
+                                    it.key.setImageResource(R.drawable._1z_x7ojlvl)
+                                }
                                 selected.clear()
+                                map.forEach {
+                                    it.key.isClickable = true
+                                }
                             }, 500)
-
                         }
                         else{
-                            Toast.makeText(this@GameActivity, "Nice", Toast.LENGTH_SHORT).show()
                             selected.clear()
+                            right += 1
                         }
+                    }
+                    if(right == difficulty){
+                        Toast.makeText(this@GameActivity, "You win", Toast.LENGTH_SHORT).show()
                     }
                 }
             }
         }
     }
-    private fun randomNums() = List(8){it}.shuffled()
+    private fun randomNums(size: Int) = List(size){it}.shuffled()
 }
